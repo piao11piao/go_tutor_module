3f65826d354fe1b57df2cf778a96da1312b717a3
piao11piao <piao11piao@gmail.com>
2023-06-11 15:08:55 +0000

    malformed change

--- hello/hello.go
+++ hello/hello.go
@@ -3,7 +3,7 @@ package main
 import (
     "fmt"
 
-    "piao11piao/go_tutor_module/greetings"
+    "github.com/piao11piao/go_tutor_module/greetings"
     //这里是引入greetings这个module，我们之前创建的。但是由于greetings这个module不在标准库里，所以暂时是找不到的
 )
 
